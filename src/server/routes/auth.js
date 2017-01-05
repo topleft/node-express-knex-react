11835734811c6d6d61c88b1c1619925523691103
Pete Jeffryes <pete@flowhub.co>
2017-01-04 18:22:12 -0700

    returning user on login to be used in client state

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -23,17 +23,14 @@ router.post('/register', authHelpers.preventLoginSignup, (req, res, next)  => {
           console.log(err);
           return next(err);
         }
-        return res.send({status: 200, message: `Success, ${user[0].username} is now registered`});
+        return res.json({status: 200, message: `Success, ${user[0].username} is now registered`});
       })(req, res, next);
     })
     .catch((err) => {
-      if (err.constraint === 'users_username_unique') {
-        err.message = 'username is already taken';
-      }
       if (err) {
-        res.send({err});
+        res.json(err);
       } else {
-        res.render('error', {err});
+        res.json({status: 500, message: 'Regsitration failed'});
       }
     });
 });
@@ -42,13 +39,19 @@ router.post('/register', authHelpers.preventLoginSignup, (req, res, next)  => {
 router.post('/login', passport.authenticate('local', {
     failureRedirect: '/auth/loginFailed'
   }), (req, res) => {
-    res.json({status: 200, message: 'success'})
+    let user = Object.assign({}, {id: req.user.id, username: req.user.username})
+    res.json({status: 200, message: 'success', user: user})
   });
 
 router.get('/logout', authHelpers.checkAuthentication, (req,res) => {
   // req.logout added by passport - delete the user id/session
   req.logout();
   res.redirect('/');
+});
+
+router.get('/current_user', authHelpers.checkAuthentication, (req,res) => {
+  //
+  res.json(req.user)
 });
 
 module.exports = router;

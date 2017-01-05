886e0c63acae6ad164dffd0d2c6bf2bc9129a7af
Pete Jeffryes <pete@flowhub.co>
2017-01-04 17:29:10 -0700

    sending auth failed json response

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -10,33 +10,37 @@ router.get('/spacegoat', function (req, res, next) {
   console.log('here?');
   return res.send({message: 'Welcome to Space Goat!!!'})
 });
-//
-// router.post('/register', authHelpers.preventLoginSignup, (req, res, next)  => {
-//   passwordHelpers.createUser(req)
-//     .then((user) => {
-//       req.login(user[0], (err)  => {
-//         if (err) {
-//           console.log(err);
-//           return next(err);
-//         }
-//         return res.send({status: 200, message: `Success, ${user[0].username} is now registered`});
-//       })(req, res, next);
-//     })
-//     .catch((err) => {
-//       if (err.constraint === 'users_username_unique') {
-//         err.message = 'username is already taken';
-//       }
-//       if (err) {
-//         res.send({err});
-//       } else {
-//         res.render('error', {err});
-//       }
-//     });
-// });
+
+router.get('/loginFailed', function (req, res, next) {
+  return res.send({status: 401, message: 'Authentication failed.'})
+});
+
+router.post('/register', authHelpers.preventLoginSignup, (req, res, next)  => {
+  passwordHelpers.createUser(req)
+    .then((user) => {
+      req.login(user[0], (err)  => {
+        if (err) {
+          console.log(err);
+          return next(err);
+        }
+        return res.send({status: 200, message: `Success, ${user[0].username} is now registered`});
+      })(req, res, next);
+    })
+    .catch((err) => {
+      if (err.constraint === 'users_username_unique') {
+        err.message = 'username is already taken';
+      }
+      if (err) {
+        res.send({err});
+      } else {
+        res.render('error', {err});
+      }
+    });
+});
 
 // authenticate users when logging in - no need for req,res passport does this for us
 router.post('/login', passport.authenticate('local', {
-    failureRedirect: '/auth/spacegoat'
+    failureRedirect: '/auth/loginFailed'
   }), (req, res) => {
     res.json({status: 200, message: 'success'})
   });
